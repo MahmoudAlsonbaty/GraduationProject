6489b3aee447a08ea7efbca570b855b8e2217b5c
Mahmoud Alsonbaty <109487998+MahmoudAlsonbaty@users.noreply.github.com>
2025-06-13 23:48:08 +0300

    f

--- Prescription Recognition Script/prescriptionScanScript.py
+++ Prescription Recognition Script/prescriptionScanScript.py
@@ -172,7 +172,7 @@ class CameraApp:
                 if frame is not None:
                     # Ensure frame has enough dimensions before converting and resizing
                     if frame.ndim >= 2:
-                        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_YUV420p2RGB)
+                        rgb_frame = frame
                         self.frame = rgb_frame # Store for potential later use if needed
                         img = Image.fromarray(rgb_frame)
                         imgtk = ImageTk.PhotoImage(image=img)
